93a65f87cc206be1c9a0b5aaa77e746fa2f9c6ad
Azure-Agst <August712@protonmail.com>
2019-04-06 18:06:28 -0400

    Refactor codebase for Maven support

--- src/main/java/pw/azureagst/buildoff/BuildMain.java
+++ src/main/java/pw/azureagst/buildoff/BuildMain.java
@@ -6,9 +6,16 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.time.Instant;
 
 public class BuildMain implements CommandExecutor {
-    private boolean isActive = false;
+    private static long startTime = 0;
+    private static long endTime = 0;
+    private static int timeLeft = 0;
+    private boolean eventIsActive = false;
     private Main main;
 
     BuildMain(Main main){
@@ -38,8 +45,37 @@ public class BuildMain implements CommandExecutor {
         // Main switch
         switch (args[0]){
             case "start":
+                if (eventIsActive){
+                    server.broadcastMessage(main.prefix + "There's already an ongoing round!");
+                    return true;
+                }
+
                 server.broadcastMessage(main.prefix + player.getPlayerListName() + " Started the next round!");
-                // TODO: Add support for starting rounds
+
+                // Set global vars
+                eventIsActive = true;
+                startTime = Instant.now().toEpochMilli();
+                endTime = startTime + (15 * 60000);
+
+                // Main Timer
+                new BukkitRunnable(){
+                    @Override
+                    public void run() {
+                        // Do time math
+                        long curtime = startTime = Instant.now().toEpochMilli();
+                        BuildMain.timeLeft = (int)(BuildMain.endTime - curtime)/1000;
+
+                        //10 minutes left
+                        if (BuildMain.timeLeft == 10*60){
+                            // Alert users via title
+                        } else if (BuildMain.timeLeft == 5*60){
+                            // Alert users via title
+                        }
+                    }
+                }.runTaskTimer(main, 20,20);
+
+
+
             case "help":
                 // TODO: Create Help Command
                 break;
